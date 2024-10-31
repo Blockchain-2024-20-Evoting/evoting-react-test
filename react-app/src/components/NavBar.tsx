de402add7b4e57d055176ba2d2799e95962e8d00
Mbgg08 <Mgutierrezg11@upao.edu.pe>
2024-10-31 12:58:18 -0500

    Actualizacion NavBar

--- react-app/src/components/NavBar.tsx
+++ react-app/src/components/NavBar.tsx
@@ -1,76 +1,173 @@
-import { useState } from "react";
-type Props = {
-  name: string;
-  path?: string;
-  navItems: string[];
-};
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
-function NavBar({ name, path, navItems }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+import React from "react";
 
-  return (
-    <nav className="navbar navbar-expand-md fixed-top navbar-light bg-light shadow">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img
-            src={path}
-            alt=""
-            width="30"
-            height="24"
-            className="d-inline-block align-center"
-          />
-          <span className="fw-bolder fs-4 ">{name}</span>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse align-items-start d-flex flex-column flex-md-row"
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav me-auto mb-2 mb-md-1">
-            {navItems.map((items, index) => (
-              <li
-                key={items}
-                className="nav-item"
-                onClick={() => setSelectedIndex(index)}
-              >
-                <a
-                  className={
-                    selectedIndex == index
-                      ? "nav-link active fw-bold"
-                      : "nav-link"
-                  }
-                  href="#"
-                >
-                  {items}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <form className="d-flex me-3">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  );
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const pages = [
+  { label: "Inicio", href: "/" },
+  { label: "Votaciones", href: "/votaciones" },
+  { label: "Estadisticas", href: "/estadisticas" },
+  { label: "Elecciones", href: "/elecciones" },
+];
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function toCamelCase(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) =>
+      index === 0 ? word.toLowerCase() : word.toUpperCase()
+    )
+    .replace(/\s+/g, "");
 }
 
-export default NavBar;
+export const NavBar: React.FC<{}> = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background: "#FFFFFF",
+        borderBottomLeftRadius: "30px",
+        borderBottomRightRadius: "30px",
+        overflow: "hidden",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none", // quita el subrayado del enlace
+              "& .logo": {
+                width: "90px", // ajusta el tamaño de la imagen
+                height: "60px",
+                marginRight: "8px", // espacio entre el logo y el texto
+              },
+            }}
+          >
+            <img className="logo" src="/src/assets/logo.svg" alt="Logo" />
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.label} onClick={handleCloseNavMenu}>
+                  <Typography
+                    component="a"
+                    href={page.href}
+                    sx={{ textAlign: "center" }}
+                  >
+                    {toCamelCase(page.label)}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.label}
+                onClick={handleCloseNavMenu}
+                href={page.href}
+                sx={{ my: 2, color: "#000000", display: "block" }}
+              >
+                {toCamelCase(page.label)}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: "center" }}>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
--- react-app/src/components/RouterLayout.tsx
+++ react-app/src/components/RouterLayout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavBarV2 } from "./NavBarV2";
+import { NavBar } from "./NavBar";
 import { Outlet } from "react-router-dom";
 
 export const RouterLayout: React.FC<{}> = () => {
   return (
     <>
-      <NavBarV2 />
+      <NavBar/>
       <Outlet />
     </>
   );
