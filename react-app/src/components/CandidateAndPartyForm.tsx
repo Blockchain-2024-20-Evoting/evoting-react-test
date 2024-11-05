e98a3943144f60dc0dd2892ce34f5e91b6fdbae2
LeTemp2806 <sebastianjoserc@gmail.com>
2024-11-05 14:20:45 -0500

    feature/coneccionDeCandidateAndParty

--- react-app/src/components/CandidateAndPartyForm.tsx
+++ react-app/src/components/CandidateAndPartyForm.tsx
@@ -1,213 +1,223 @@
-// src/components/CandidateAndPartyForm.tsx
-import React, { useEffect, useState } from 'react';
-import { Box, Typography, TextField, Button, Grid, IconButton, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const CandidateAndPartyForm: React.FC = () => {
-  const [election, setElection] = React.useState('');
-  const [visible, setVisible] = useState(false);
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [candidateImage, setCandidateImage] = useState<File | null>(null);
+  const [partyName, setPartyName] = useState<string>('');
+  const [partyDescription, setPartyDescription] = useState<string>('');
+  const [partyImage, setPartyImage] = useState<File | null>(null);
+  const [election, setElection] = useState<string>('');
+  const [elections, setElections] = useState<any[]>([]);
 
   useEffect(() => {
-    // Hacer que el formulario sea visible después de que se monte el componente
-    setVisible(true);
+    const fetchElections = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/v1/election');
+        setElections(response.data);
+      } catch (error) {
+        console.error('Error fetching elections:', error);
+      }
+    };
+    fetchElections();
   }, []);
 
-  const handleElectionChange = (event: SelectChangeEvent) => {
-    setElection(event.target.value as string);
+  const handleCreate = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Verificación de campos obligatorios
+    if (!firstName || !lastName || !election || !partyName || !partyDescription) {
+      alert('Por favor, complete todos los campos obligatorios.');
+      return;
+    }
+
+    // Comprobar si las imágenes están seleccionadas
+    if (!candidateImage) {
+      alert('Por favor, seleccione una imagen para el candidato.');
+      return;
+    }
+
+    if (!partyImage) {
+      alert('Por favor, seleccione una imagen para el partido.');
+      return;
+    }
+
+    const candidateFormData = new FormData();
+    candidateFormData.append('firstName', firstName);
+    candidateFormData.append('lastName', lastName);
+    candidateFormData.append('electionId', election);
+    candidateFormData.append('img', candidateImage);
+
+    const partyFormData = new FormData();
+    partyFormData.append('name', partyName);
+    partyFormData.append('description', partyDescription);
+    partyFormData.append('img', partyImage);
+
+    try {
+
+      // Envío de los datos del partido
+      const partyResponse = await axios.post('http://localhost:8080/v1/party', partyFormData, {  
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log("Partido creado:", partyResponse.data.id);
+      
+      candidateFormData.append('partyId', partyResponse.data.id);
+
+      // Envío de los datos del candidato
+      const candidateResponse = await axios.post('http://localhost:8080/v1/candidate', candidateFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+         },
+      });
+
+      console.log("Candidato creado:", candidateResponse.data);
+
+      // Resetear campos después de crear
+      setFirstName('');
+      setLastName('');
+      setPartyName('');
+      setPartyDescription('');
+      setElection('');
+      setCandidateImage(null);
+      setPartyImage(null);
+
+      
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error:', error);
+        alert(`Error al crear candidato o partido: ${error.response?.data?.message || 'Error desconocido'}`);
+      } else {
+        console.error('Error:', error);
+        alert('Error desconocido al crear candidato o partido.');
+      }
+    }
   };
 
   return (
-    <Box
-      sx={{
-        background: 'linear-gradient(to bottom, #290E55, #7E4DCF)', // Degradado de fondo
-        padding: 4,
-        borderRadius: 10,
-        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-        width: 'calc(100% - 20px)', // Ancho total menos 20px de margen
-        transition: 'opacity 0.5s ease, transform 0.5s ease',
-        opacity: visible ? 1 : 0,
-        transform: visible ? 'translateY(0)' : 'translateY(20px)',
+    <form
+      className="candidate-party-form"
+      onSubmit={handleCreate}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        backgroundColor: '#f9f9f9',
       }}
     >
-      {/* Caja interna para el contenido */}
-      <Box
-        sx={{
-          background: '#fff',
-          padding: 4,
-          borderRadius: 2,
-          boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
-          border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
+      <h2 className="form-title" style={{ fontSize: '1.5rem', marginBottom: '10px' }}>Crear Candidato</h2>
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="firstName">Nombre del candidato</label>
+        <input
+          type="text"
+          id="firstName"
+          placeholder="Nombre del candidato"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        />
+      </div>
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="lastName">Apellido del candidato</label>
+        <input
+          type="text"
+          id="lastName"
+          placeholder="Apellido del candidato"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        />
+      </div>
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="candidateImage">Imagen del candidato</label>
+        <input
+          type="file"
+          id="candidateImage"
+          accept="image/*"
+          onChange={(e) => e.target.files && setCandidateImage(e.target.files[0])}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        />
+      </div>
+
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="election">Elección</label>
+        <select
+          id="election"
+          value={election}
+          onChange={(e) => setElection(e.target.value)}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        >
+          <option value="">Selecciona una elección</option>
+          {elections.map((election) => (
+            <option key={election.id} value={election.id}>
+              {election.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <h2 className="form-title" style={{ fontSize: '1.5rem', marginBottom: '10px' }}>Crear Partido</h2>
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="partyName">Nombre del partido</label>
+        <input
+          type="text"
+          id="partyName"
+          placeholder="Nombre del partido"
+          value={partyName}
+          onChange={(e) => setPartyName(e.target.value)}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        />
+      </div>
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="partyDescription">Descripción del partido</label>
+        <input
+          type="text"
+          id="partyDescription"
+          placeholder="Descripción del partido"
+          value={partyDescription}
+          onChange={(e) => setPartyDescription(e.target.value)}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        />
+      </div>
+      <div className="form-group" style={{ display: 'flex', flexDirection: 'column' }}>
+        <label htmlFor="partyImage">Imagen del partido</label>
+        <input
+          type="file"
+          id="partyImage"
+          accept="image/*"
+          onChange={(e) => e.target.files && setPartyImage(e.target.files[0])}
+          required
+          style={{ padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="submit-button"
+        style={{
+          padding: '10px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
         }}
       >
-        {/* Datos del Candidato */}
-        <Grid container spacing={2} sx={{ border: '1px solid rgba(0, 0, 0, 0.12)', borderRadius: 1, padding: 2 }}>
-          <Grid item xs={12}>
-            <Typography variant="h5" gutterBottom color="#47184D" textAlign="center"> {/* Color cambiado */}
-              Datos candidato
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box textAlign="center">
-              <IconButton
-                color="primary"
-                aria-label="upload picture"
-                component="label"
-                sx={{
-                  borderRadius: '12px', // Esquinas redondeadas
-                  background: 'linear-gradient(to bottom, #290E55, #7E4DCF)', // Degradado
-                  boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.2)', // Sombreado
-                  '&:hover': {
-                    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.3)', // Sombreado más intenso al pasar el mouse
-                  },
-                }}
-              >
-                <input hidden accept="image/*" type="file" />
-                <PhotoCamera sx={{ fontSize: 80, color: '#fff' }} />
-              </IconButton>
-              <Button
-                variant="outlined"
-                component="label"
-                sx={{
-                  marginTop: 1,
-                  border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                  boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.2)', // Sombreado
-                  color: 'black', // Color del texto en negro
-                  '&:hover': {
-                    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.3)', // Sombreado más intenso al pasar el mouse
-                  },
-                }}
-              >
-                subir imagen
-                <input hidden accept="image/*" type="file" />
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <TextField
-              label="Nombres"
-              fullWidth
-              margin="normal"
-              InputProps={{
-                sx: {
-                  border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                  borderRadius: '4px', // Esquinas redondeadas
-                },
-              }}
-            />
-            <TextField
-              label="Apellidos"
-              fullWidth
-              margin="normal"
-              InputProps={{
-                sx: {
-                  border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                  borderRadius: '4px', // Esquinas redondeadas
-                },
-              }}
-            />
-            <Box mt={2}>
-              <InputLabel id="election-label">Elecciones</InputLabel>
-              <Select
-                labelId="election-label"
-                value={election}
-                onChange={handleElectionChange}
-                fullWidth
-                sx={{
-                  '& .MuiSelect-select': {
-                    border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                    borderRadius: '4px', // Esquinas redondeadas
-                  },
-                }}
-              >
-                <MenuItem value="">
-                  <em>Seleccione una opción</em>
-                </MenuItem>
-                <MenuItem value="eleccion1">Elección 1</MenuItem>
-                <MenuItem value="eleccion2">Elección 2</MenuItem>
-              </Select>
-            </Box>
-          </Grid>
-        </Grid>
-
-        {/* Datos del Partido */}
-        <Grid container spacing={2} sx={{ border: '1px solid rgba(0, 0, 0, 0.12)', borderRadius: 1, padding: 2, mt: 4 }}>
-          <Grid item xs={12}>
-            <Typography variant="h5" gutterBottom color="#47184D" textAlign="center"> {/* Color cambiado */}
-              Datos Partido
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box textAlign="center">
-              <IconButton
-                color="primary"
-                aria-label="upload picture"
-                component="label"
-                sx={{
-                  borderRadius: '12px', // Esquinas redondeadas
-                  background: 'linear-gradient(to bottom, #290E55, #7E4DCF)', // Degradado
-                  boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.2)', // Sombreado
-                  '&:hover': {
-                    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.3)', // Sombreado más intenso al pasar el mouse
-                  },
-                }}
-              >
-                <input hidden accept="image/*" type="file" />
-                <PhotoCamera sx={{ fontSize: 80, color: '#fff' }} />
-              </IconButton>
-              <Button
-                variant="outlined"
-                component="label"
-                sx={{
-                  marginTop: 1,
-                  border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                  boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.2)', // Sombreado
-                  color: 'black', // Color del texto en negro
-                  '&:hover': {
-                    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.3)', // Sombreado más intenso al pasar el mouse
-                  },
-                }}
-              >
-                subir imagen
-                <input hidden accept="image/*" type="file" />
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <TextField
-              label="Nombre partido"
-              fullWidth
-              margin="normal"
-              InputProps={{
-                sx: {
-                  border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                  borderRadius: '4px', // Esquinas redondeadas
-                },
-              }}
-            />
-            <TextField
-              label="Descripción"
-              fullWidth
-              multiline
-              rows={4}
-              margin="normal"
-              InputProps={{
-                sx: {
-                  border: '1px solid rgba(0, 0, 0, 0.12)', // Borde sutil
-                  borderRadius: '4px', // Esquinas redondeadas
-                },
-              }}
-            />
-          </Grid>
-        </Grid>
-
-        {/* Botón Guardar */}
-        <Box textAlign="center" mt={4}>
-          <Button variant="contained" sx={{ backgroundColor: '#F8F8F8', color: 'black', textTransform: 'lowercase' }}> {/* Fondo blanco y texto en minúsculas */}
-            guardar candidato
-          </Button>
-        </Box>
-      </Box>
-    </Box>
+        Crear Candidato y Partido
+      </button>
+    </form>
   );
 };
 
